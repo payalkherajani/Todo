5f543a1e3ef0087b103a6ace6b0613d014452c13
payalkherajani <payal.kherajani19@gmail.com>
2020-10-10 18:33:08 +0530

    Register-page-Styling-done

--- src/routes/SignUp/index.js
+++ src/routes/SignUp/index.js
@@ -3,30 +3,32 @@ import React from "react";
 const Register = () => {
 
   return (
-    <form>
-      <div>
-        <div>
-          <h1>register</h1>
+    <div className="Register-main-div">
+    <form className="form-div">
+      <div className="container-register">
+        <div className="heading-container">
+          <h1 className="heading-register">register</h1>
         </div>
 
-        <div>
-          <input  type="text" placeholder="Full Name"/>
+        <div className="form-input">
+          <input className="input-register" type="text" placeholder="Full Name"/>
         </div>
 
-        <div>
-          <input  type="email" placeholder="Email"/>
+        <div className="form-input">
+          <input className="input-register"  type="email" placeholder="Email"/>
         </div>
 
-        <div >
-          <input type="number" placeholder="Phone Number"/>
+        <div className="form-input" >
+          <input className="input-register" type="number" placeholder="Phone Number"/>
         </div>
-        <div>
-          <button>
+        <div className="form-button-div">
+          <button className="register-button">
             Register
           </button>
         </div>
       </div>
     </form>
+    </div>
   );
 };
 
